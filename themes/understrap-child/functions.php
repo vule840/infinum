b0547101177a8f4b34814d4c6aee849c227ace48
vule840 <vule840@gmail.com>
2018-04-15 17:16:07 +0200

    Dodao sam shortcode
    Shortcode za quotove i custom posty type,

--- themes/understrap-child/functions.php
+++ themes/understrap-child/functions.php
@@ -51,3 +51,76 @@ add_filter('the_tags','add_class_the_tags');
             'before_title'  => '<h3 class="widget-title">',
             'after_title'   => '</h3>',
         ) );
+
+
+//add new unicorn post type
+
+ if ( ! function_exists('better_unicorn') ) {
+
+// Register Custom Post Type
+function better_unicorn() {
+
+    $labels = array(
+        'name'                  => _x( 'better_unicorns', 'Post Type General Name', 'text_domain' ),
+        'singular_name'         => _x( 'better_unicorn', 'Post Type Singular Name', 'text_domain' ),
+        'menu_name'             => __( 'better unicorn', 'text_domain' ),
+        'name_admin_bar'        => __( 'New unicorns', 'text_domain' ),
+        'archives'              => __( 'Item Archives', 'text_domain' ),
+        'attributes'            => __( 'Item Attributes', 'text_domain' ),
+        'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+        'all_items'             => __( 'All Items', 'text_domain' ),
+        'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+        'add_new'               => __( 'Add New unicorn', 'text_domain' ),
+        'new_item'              => __( 'New Item', 'text_domain' ),
+        'edit_item'             => __( 'Edit Item', 'text_domain' ),
+        'update_item'           => __( 'Update Item', 'text_domain' ),
+        'view_item'             => __( 'View Item', 'text_domain' ),
+        'view_items'            => __( 'View Items', 'text_domain' ),
+        'search_items'          => __( 'Search Item', 'text_domain' ),
+        'not_found'             => __( 'Not found', 'text_domain' ),
+        'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+        'featured_image'        => __( 'Featured Image', 'text_domain' ),
+        'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+        'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+        'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+        'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+        'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+        'items_list'            => __( 'Items list', 'text_domain' ),
+        'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+        'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+    );
+    $args = array(
+        'label'                 => __( 'better_unicorn', 'text_domain' ),
+        'description'           => __( 'better_unicorn', 'text_domain' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title', 'editor' ),
+        'taxonomies'            => array( 'category', 'post_tag' ),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 5,
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'page',
+    );
+    register_post_type( 'better_unicorn', $args );
+
+}
+add_action( 'init', 'better_unicorn', 0 );
+
+} 
+
+
+
+add_shortcode('my_blockquote', 'my_blockquote');
+function my_blockquote($atts, $content) {
+    return '<div class="span3 quote well">'.PHP_EOL
+        .'<i class="icon-quote-left icon-2x pull-left icon-muted"></i>'.PHP_EOL
+        .'<blockquote class="lead">'.$content.'</blockquote>'.PHP_EOL
+        .'</div>';
+}      
